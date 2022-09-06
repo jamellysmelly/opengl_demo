f2298a5021eaad210636338e55879ce04cd1be93
Jamal Royce Smith <jamalsmith95.bc@gmail.com>
2022-09-05 22:01:12 -0600

    Remove Windows support.

--- build_deps.py
+++ build_deps.py
@@ -19,7 +19,7 @@ import sys
 import os
 
 main_dir = os.getcwd()
-glew_build_dir = "build/glew/build/cmake/build/"
+glew_build_dir = "build/glew/"
 sdl2_build_dir = "build/SDL2/build/"
 glew_done = "GLEW is already built."
 sdl2_done = "SDL2 is already built."
@@ -37,11 +37,9 @@ def build_linux_deps():
             os.system(linux_copy_glew)
             
             ## Change directory to glew/build
-            os.mkdir(glew_build_dir)
             os.chdir(glew_build_dir)
             
             ## Run the cmake build script and build it with the makefile output 
-            os.system(cmake_command)
             os.system(make_command)
         except Exception as e: 
             print(repr(e))
@@ -66,57 +64,10 @@ def build_linux_deps():
             print(repr(e))
     else:
         print(sdl2_done)
-        
-def build_windows_deps():
-    windows_copy_glew = "Xcopy /E /I deps\glew build\glew"
-    windows_copy_sdl2 = "Xcopy /E /I deps\SDL2 build\SDL2"
-
-    ##cmake_command = "cmake -S ../ -B ./ -G \"MinGW Makefiles\" -DCMAKE_BUILD_TYPE=Release"
-    ## mingw32_make_cmd = "mingw32-make.exe -j{}".format(os.cpu_count())
-    cmake_command = "cmake -S ../ -B ./ -G \"Ninja\" -DCMAKE_BUILD_TYPE=Release"
-    mingw32_make_cmd = "ninja"
-
-    if os.path.isdir(glew_build_dir) == False:
-        try:
-            ## Copy glew to build
-            os.system(windows_copy_glew)
-            
-            ## Create and change directory to glew/build
-            os.mkdir(glew_build_dir)
-            os.chdir(glew_build_dir)
-            
-            ## Run the cmake build script and build it with the msbuild output.
-            os.system(cmake_command)
-            os.system(mingw32_make_cmd)
-        except Exception as e: 
-            print(repr(e))
-    else:
-        print(glew_done)
-        
-    ## Change back to the main folder.
-    os.chdir(main_dir)
-    if os.path.isdir(sdl2_build_dir) == False:
-        try:
-            ## Copy SDL2 to build.
-            os.system(windows_copy_sdl2)
-        
-            ## Make and change directoy to SDL2/build
-            os.mkdir(sdl2_build_dir)
-            os.chdir(sdl2_build_dir)
-
-            ## Run the cmake build script and build it with the msbuild output.
-            os.system(cmake_command)
-            os.system(mingw32_make_cmd)
-        except Exception as e: 
-            print(repr(e))
-    else:
-        print(sdl2_done)
-        
+             
 def main():
     print("Python version: " + (sys.version))
     if sys.platform == "linux" or sys.platform == "linux2":
         build_linux_deps()
-    elif sys.platform == "win32":
-        build_windows_deps()
-        
+
 main()
--- docs/build_instructions.md
+++ docs/build_instructions.md
@@ -8,45 +8,19 @@
 * Dear ImGui v1.88 (Provided via modified source in deps/imgui/.) 
 ( https://github.com/ocornut/imgui/)
 
-## Make sure to install the following dependencies.
-### Cross platform build dependencies:
-* CMake >=v3.20.2 (https://cmake.org/)
-* OpenGL >=v4.3.0 (Provided via graphics drivers.)
-* Python >=v3.9.10 (Used in cmake script to compile dependencies.) 
-( https://python.org/.)
-
-
-### Linux specific build dependencies:
+### External build dependencies:
 * GNU Make >=v4.2
 * C/C++ Compiler (>=GNU GCC 11.1.1, or >= LLVM Clang 13.0.0)
-*
+* CMake >=v3.20.2 (https://cmake.org/)
+* OpenGL >=v4.3.0 (Provided via graphics drivers.)
+* Python >=v3.9.10 (Used in cmake script to pre compile some dependencies.)
+( https://python.org/.)
 * Please see [deps/glew/README.md](../deps/glew/README.md) and
 [deps/SDL2/doscs/README-linux.md](../deps/SDL2/docs/README-linux.md) for
 additional development packages.
 
-### Windows specific build dependencies:
-* Ninja >=1.10.0
-* C/C++ Compiler (>=MinGW-W64 GNU GCC 11.1.1, or >=MinGW-W64 LLVM Clang 13.0.0)
-*
-* Optionally, you can install __scoop__ (An open source powershell package manager 
-for Windows) from (https://scoop.sh/.) 
-* Then you can use scoop to install __mingw-winlibs-llvm-ucrt__, 
- __cmake__, and __python__ to aquire all above mentioned dependencies.
-*
-* __Please note__, this project only supports compiling via MinGW-W64 on Windows
-at this time.
-
-## Linux build instructions:
+## Build instructions:
 * export CC="gcc" or export CC="clang"
 * export CXX="g++" or export CXX="clang++"
 * mkdir build && cd build
 * cmake -DCMAKE_BUILD_TYPE=Release .. && make -j4
-
-## Windows build instructions:
-* $env:CC="gcc" or $env:CC=="clang"
-* $env:CXX="g++" or $env:CXX="clang++"
-* mkdir build
-* cd build
-* cmake -S ../ -B ./ -G "Ninja" -DCMAKE_BUILD_TYPE=Release
-* ninja
-
